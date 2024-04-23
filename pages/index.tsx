760293e2f92e34f2988df89a6af9253fc50354e6
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-04-23 02:01:40 -0400

    few changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter } from "next/router";
-import OnboardingSteps from "../components/OnboardingSteps";
 // import { useState, useEffect } from "react";
 
 function Project() {
